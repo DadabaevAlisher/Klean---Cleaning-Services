3a5851e856f2e69f1192a4d111f019081a9d7341
iCloud <dadabaev027@gmail.com>
2023-03-13 22:13:32 +0500

    add post with database

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -11,7 +12,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::all();
+
+        return view('posts.index')->with('posts', $posts);
     }
 
     /**
@@ -19,7 +22,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        // 
     }
 
     /**
